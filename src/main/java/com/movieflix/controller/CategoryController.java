c02673adcdd87c64bf90c1c8a501ac34d4646df3
arthurvasc <arthurvasc2@gmail.com>
2025-02-26 19:31:05 -0300

    feat: add logic to CategoryService and routes to CategoryController

--- src/main/java/com/movieflix/controller/CategoryController.java
+++ src/main/java/com/movieflix/controller/CategoryController.java
@@ -1,14 +1,44 @@
 package com.movieflix.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.movieflix.entity.Category;
+import com.movieflix.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("movieflix/category")
 public class CategoryController {
-    @GetMapping("/category")
-    public String helloWorld(){
-        return "Hello World.";
+
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
     }
+
+    @GetMapping()
+    public List<Category> findAll(){
+      return categoryService.findAll();
+    }
+
+    @GetMapping("/list/{id}")
+    public Category findById(@PathVariable Long id){
+        return categoryService.findById(id);
+
+    }
+
+    @PostMapping("/create")
+    public Category createCategory(@RequestBody Category category){
+        return categoryService.saveCategory(category);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteCategory(@PathVariable Long id){
+        categoryService.deleteCategoryById(id);
+    }
+
+
+
 }
--- src/main/java/com/movieflix/service/CategoryService.java
+++ src/main/java/com/movieflix/service/CategoryService.java
@@ -1,13 +1,38 @@
 package com.movieflix.service;
 
+import com.movieflix.entity.Category;
 import com.movieflix.repository.CategoryRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
-    @Autowired
-    CategoryRepository categoryRepository;
 
+    private final CategoryRepository repository;
+
+    public CategoryService(CategoryRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Category> findAll(){
+        return repository.findAll();
+    }
+
+    public Category findById(Long id){
+        Optional<Category> categoryFound = repository.findById(id);
+        return categoryFound.orElse(null);
+
+    }
+
+    public Category saveCategory(Category category){
+        return repository.save(category);
+    }
+
+    public void deleteCategoryById (Long id){
+        repository.deleteById(id);
+        System.out.println("Usuario com o id: " + id + " deletado com sucesso.");
+    }
 
 }
