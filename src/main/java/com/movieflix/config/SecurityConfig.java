a0bd4ee272ed503384aca98373826a3455c61bda
arthurvasc <arthurvasc2@gmail.com>
2025-03-13 15:13:05 -0300

    feat: adds the securityFilter before the request and the authentication of the token bearer

--- src/main/java/com/movieflix/config/SecurityConfig.java
+++ src/main/java/com/movieflix/config/SecurityConfig.java
@@ -13,11 +13,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final SecurityFilter securityFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -31,7 +34,7 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
 
                 )
-
+               .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                .build();
 
     }
--- src/main/java/com/movieflix/config/TokenService.java
+++ src/main/java/com/movieflix/config/TokenService.java
@@ -2,12 +2,15 @@ package com.movieflix.config;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.movieflix.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
+import java.util.Optional;
 
 
 @Component
@@ -28,5 +31,25 @@ public class TokenService {
                 .withExpiresAt(Instant.now().plusSeconds(86400))
                 .withIssuer("API Movieflix")
                 .sign(Algorithm.HMAC256(secret));
+    }
+
+    public Optional<JWTUserData> verifyToken(String token){
+        try {
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+
+        DecodedJWT jwt = JWT.require(algorithm)
+                .build()
+                .verify(token);
+
+        return Optional.of(JWTUserData
+                .builder()
+                .id(jwt.getClaim("userId").asLong())
+                .name(jwt.getClaim("name").asString())
+                .email(jwt.getSubject())
+                .build());
+
+        } catch (JWTVerificationException ex) {
+            return Optional.empty();
+        }
     }
 }
